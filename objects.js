90f731ec4537231e956d03427a7315d52afc9730
Christian Weiss <worldmover777@gmail.com>
2018-01-28 03:12:12 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,5 +1,10 @@
 var playlist = {artist:"song"}
 
 function updatePlaylist(playlist, artistName, songTitle){
- Object.assign({}, playlist, {[artistName]:songTitle}) 
+ Object.assign({}, playlist, {[artistName]:songTitle})
+ return playlist
+}
+
+function removeFromPlaylist(playlist, artistName){
+  
 }
